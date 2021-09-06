ea15763c6d79fbf4850bd5ed7422b06cbf055d4b
= <=>
2021-09-06 10:28:39 +0200

    The cards are made

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const d = document;
 
-function pokePic(pic, hp, exp, att, satt, def, name) {
-    let pokeName = d.getElementById('pokeName');
-    let pokeImg = d.getElementById('pokePic');
-    let pokeHP = d.getElementById('pokeHP');
-    let pokeExp = d.getElementById('pokeExp');
+function pokePic_1(pic, hp, exp, att, satt, def, name) {
+    let pokeName = d.querySelector('.pokeName');
+    let pokeImg = d.querySelector('.img');
+    let pokeHP = d.querySelector('.pokeHP');
+    let pokeExp = d.querySelector('.pokeExp');
     let pokeAttack = d.querySelector('.card_2_sub1_attack');
     let pokeSpecialAttack = d.querySelector('.card_2_sub2_specialAttack');
     let pokeDefense = d.querySelector('.card_2_sub3_defense');
@@ -32,12 +32,15 @@ if (randomNum === 4) {
     img.setAttribute('style', 'margin-left: 30px;')
 }
 
+
 fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
     .then(function(response) {
         // debugger;
         return response.json();
     })
     .then(pokeJSON => {
+        console.log('Primera respuesta: ')
+
         console.log(pokeJSON);
 
         let pic = pokeJSON.sprites.other.dream_world.front_default;
@@ -50,6 +53,122 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
         let name = pokeJSON.name;
 
 
-        pokePic(pic, hp, exp, att, satt, def, name);
+        pokePic_1(pic, hp, exp, att, satt, def, name);
 
+    });
+
+
+// SEGUNDA TARJETA
+
+let secondCardRandomNum = Math.round(Math.random() * 100) + 1;
+
+if (secondCardRandomNum === 4) {
+    let img = d.querySelector('.img2');
+    img.setAttribute('style', 'margin-left: 30px;')
+}
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${secondCardRandomNum}`)
+    .then(function(response) {
+        // debugger;
+        return response.json();
     })
+    .then(pokeJSON => {
+        console.log('Segunda respuesta: ')
+        console.log(pokeJSON);
+
+        let pic = pokeJSON.sprites.other.dream_world.front_default;
+        let hp = pokeJSON.stats[0].base_stat;
+        let exp = pokeJSON.base_experience;
+        let att = pokeJSON.stats[1].base_stat;
+        let def = pokeJSON.stats[2].base_stat;
+        let satt = pokeJSON.stats[3].base_stat;
+
+        let name = pokeJSON.name;
+
+
+        pokePic_2(pic, hp, exp, att, satt, def, name);
+
+    });
+
+
+function pokePic_2(pic, hp, exp, att, satt, def, name) {
+    let pokeName = d.querySelector('.pokeName2');
+    let pokeImg = d.querySelector('.img2');
+    let pokeHP = d.querySelector('.pokeHP2');
+    let pokeExp = d.querySelector('.pokeExp2');
+    let pokeAttack = d.querySelector('.card_2_sub1_attack2');
+    let pokeSpecialAttack = d.querySelector('.card_2_sub2_specialAttack2');
+    let pokeDefense = d.querySelector('.card_2_sub3_defense2');
+
+    pokeImg.src = `${pic}`;
+    pokeImg.setAttribute('width', '200px');
+    pokeImg.setAttribute('height', '200px');
+
+
+
+    pokeHP.textContent = `${hp}`;
+    pokeExp.textContent = `${exp}`;
+    pokeAttack.textContent = `${att}K`;
+    pokeSpecialAttack.textContent = `${satt}K`;
+    pokeDefense.textContent = `${def}K`;
+    pokeName.textContent = `${name}`;
+
+}
+
+
+// TERCERA TARJETA
+
+let thirdCardRandomNum = Math.round(Math.random() * 100) + 1;
+
+if (thirdCardRandomNum === 4) {
+    let img = d.querySelector('.img3');
+    img.setAttribute('style', 'margin-left: 30px;')
+}
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${thirdCardRandomNum}`)
+    .then(function(response) {
+        // debugger;
+        return response.json();
+    })
+    .then(pokeJSON => {
+        console.log('Tercera respuesta: ')
+        console.log(pokeJSON);
+
+        let pic = pokeJSON.sprites.other.dream_world.front_default;
+        let hp = pokeJSON.stats[0].base_stat;
+        let exp = pokeJSON.base_experience;
+        let att = pokeJSON.stats[1].base_stat;
+        let def = pokeJSON.stats[2].base_stat;
+        let satt = pokeJSON.stats[3].base_stat;
+
+        let name = pokeJSON.name;
+
+
+        pokePic_3(pic, hp, exp, att, satt, def, name);
+
+    });
+
+
+function pokePic_3(pic, hp, exp, att, satt, def, name) {
+    let pokeName = d.querySelector('.pokeName3');
+    let pokeImg = d.querySelector('.img3');
+    let pokeHP = d.querySelector('.pokeHP3');
+    let pokeExp = d.querySelector('.pokeExp3');
+    let pokeAttack = d.querySelector('.card_2_sub1_attack3');
+    let pokeSpecialAttack = d.querySelector('.card_2_sub2_specialAttack3');
+    let pokeDefense = d.querySelector('.card_2_sub3_defense3');
+
+    pokeImg.src = `${pic}`;
+    pokeImg.setAttribute('width', '200px');
+    pokeImg.setAttribute('height', '200px');
+
+
+
+    pokeHP.textContent = `${hp}`;
+    pokeExp.textContent = `${exp}`;
+    pokeAttack.textContent = `${att}K`;
+    pokeSpecialAttack.textContent = `${satt}K`;
+    pokeDefense.textContent = `${def}K`;
+    pokeName.textContent = `${name}`;
+
+}
